b9d548cb5a3243b0121c78ec52a3578785b8d501
jing.li <jing.li@yitu-inc.com>
2019-05-28 09:55:02 +0800

    sth

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/monty-research'));
+app.use(express.static( '/dist/monty-research'));
 
 app.get('/*', function(req,res) {
     
