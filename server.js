8d8bd91aeda94088a2d6ffaf136e795b08a1af2f
jing.li <jing.li@yitu-inc.com>
2019-05-28 10:50:46 +0800

    origin

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static(__dirname + '/dist/monty-research'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/monty-research/index.html'));
+res.sendFile(path.join(__dirname+'/dist/monty-research/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
